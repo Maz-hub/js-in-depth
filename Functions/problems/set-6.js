84c901cdf3a432e5a77595e35577646f647e04c8
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-05-05 13:09:06 +0000

    Add implementation of greeting function and dog years calculation

---
+++ Functions/problems/set-6.js
@@ -0,0 +1,13 @@
+/*
+Create a function that takes a person’s name as a parameter, and returns a greeting string like:
+"Hello, Maz!"
+*/
+
+const greet = (name) => {
+    return "Hello, " + name + "!"
+}
+
+const result = greet("Maz");
+
+console.log(result)
+
---
+++ Functions/problems/set-7.js
@@ -0,0 +1,25 @@
+/*
+Create a function that takes a person’s age (in human years) and returns their age in dog years.
+
+🐶 1 human year = 7 dog years
+
+✅ Requirements:
+Name the function calculateDogYears
+
+Take one parameter: humanAge
+
+Return the age multiplied by 7
+
+Store the result in a variable
+
+Use console.log() to show the dog age
+*/
+
+const calculateDogYears = (humanAge) => {
+    return humanAge * 7
+}
+
+const result = calculateDogYears(43)
+
+console.log(result);
+
