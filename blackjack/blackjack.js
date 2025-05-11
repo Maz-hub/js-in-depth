7d2b370d9ccdbfc1668012767f983de4c220227e
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-05-11 07:09:28 +0000

    Add initial setup for blackjack deck with suits and cards arrays

---
+++ blackjack/blackjack.js
@@ -0,0 +1,14 @@
+// create an empty deck to store
+const deck = [];
+
+// NOT to hard code one by one of 52 cards, as it would get tidiouse.
+//  create 2 arrays: suits and cards
+ const suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
+ const cards = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"];
+
+ // go throught an array / list of things ? Use a loop
+
+ for (const card of cards) {
+    
+ }
+
