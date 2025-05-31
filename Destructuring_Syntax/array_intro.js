a059ca7676c867f5ef3fd6f69724a65daa4416ee
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-05-31 09:52:10 +0000

    Add example demonstrating array destructuring in JavaScript

---
+++ Destructuring_Syntax/array_intro.js
@@ -0,0 +1,13 @@
+const cities = ["Vancouver", "Moscow", "Bern", "London", "Berlin"];
+
+// Before destructuring
+// const vancouver = cities[0];
+// const moscow = cities[1];
+
+// Using Destructuring
+const [, moscow] = cities;
+
+
+// console.log(vancouver, moscow);
+
+console.log(moscow);
